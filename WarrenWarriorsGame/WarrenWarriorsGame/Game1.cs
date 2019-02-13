72573259dac21acf68b1a9b05682701b1028eb4e
llp5030 <llp5030@IGMOH1375-27>
2019-02-13 09:46:49 -0500

    Kowalski, Analysis

--- WarrenWarriorsGame/WarrenWarriorsGame/Game1.cs
+++ WarrenWarriorsGame/WarrenWarriorsGame/Game1.cs
@@ -5,7 +5,7 @@ using Microsoft.Xna.Framework.Input;
 namespace WarrenWarriorsGame
 {
     /// <summary>
-    /// This is the main type for your game.
+    /// "Kowalski, Analysis" -Liam
     /// </summary>
     public class Game1 : Game
     {
