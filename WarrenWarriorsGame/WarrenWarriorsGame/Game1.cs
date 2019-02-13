9d77a053caebab974f10fb05434908f47979e096
Noah Hulick <njh9330@g.rit.edu>
2019-02-13 09:48:00 -0500

    I added a masterful comment on line 8

--- WarrenWarriorsGame/WarrenWarriorsGame/Game1.cs
+++ WarrenWarriorsGame/WarrenWarriorsGame/Game1.cs
@@ -5,7 +5,7 @@ using Microsoft.Xna.Framework.Input;
 namespace WarrenWarriorsGame
 {
     /// <summary>
-    /// This is the main type for your game.
+    /// ///Noah Hulick, Bringer of Chaos, Reporting for duty.
     /// </summary>
     public class Game1 : Game
     {
