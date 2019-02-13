fe0acdf1670c5b406acd695823ecfc8d48ef7ec2
ecb9413 <ecb9413@IGMOH1375-19>
2019-02-13 09:46:53 -0500

    Edited the comment on line 8

--- WarrenWarriorsGame/WarrenWarriorsGame/Game1.cs
+++ WarrenWarriorsGame/WarrenWarriorsGame/Game1.cs
@@ -5,7 +5,7 @@ using Microsoft.Xna.Framework.Input;
 namespace WarrenWarriorsGame
 {
     /// <summary>
-    /// This is the main type for your game.
+    /// Eddie is just a terrible person. The worst.
     /// </summary>
     public class Game1 : Game
     {
