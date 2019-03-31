825915f86fd6825a385bf3d769e2db6eb5c571c4
llp5030 <llp5030@IGM2025-03>
2019-03-31 11:24:19 -0400

    Added some code in the dungeon class

--- WarrenWarriorsGame/WarrenWarriorsGame/Dungeon.cs
+++ WarrenWarriorsGame/WarrenWarriorsGame/Dungeon.cs
@@ -3,19 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace WarrenWarriorsGame
 {
     public class Dungeon
     {
+        Random rn = new Random();
         Button[,] roomButtons;
         Encounter[,] dungeonlayout;
 
-        public Dungeon()
+        public Dungeon(Game g)
         {
-            /*
-             create the dungeon in a pseudo random method that allows the user to traverse the entire dungeon.
-             */
+            int roller;
+            roomButtons = new Button[20, 20];
+            dungeonlayout = new Encounter[20, 20];
+
+            //Creates Buttons in Array
+            for (int x = 0; x < 20; x++)
+            {
+                for (int y = 0; y < 20; y++)
+                {
+                    roller = rn.Next(0, 5);
+                    switch (roller)
+                    {
+                        case 0:
+                            dungeonlayout[x, y] = new Encounter(Difficulty.None);
+                            break;
+                        case 1:
+                            dungeonlayout[x, y] = new Encounter(Difficulty.Easy);
+                            break;
+                        case 2:
+                            dungeonlayout[x, y] = new Encounter(Difficulty.Medium);
+                            break;
+                        case 3:
+                            dungeonlayout[x, y] = new Encounter(Difficulty.Hard);
+                            break;
+                        case 4:
+                            dungeonlayout[x, y] = new Encounter(Difficulty.Boss);
+                            break;
+                    }
+
+                    //Need sprites for button modes before this can be implemented
+                    roomButtons[x, y] = new Button(g.Content.Load<Texture2D>("RoomNormal"), g.Content.Load<Texture2D>("RoomNext"), g.Content.Load<Texture2D>("RoomCleared"), new Rectangle(x*20, y*20, 15,15));
+                    
+                    
+                }
+            }
         }
         public void Update()
         {
