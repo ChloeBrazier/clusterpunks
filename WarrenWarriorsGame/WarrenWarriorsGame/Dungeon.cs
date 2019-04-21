e835e154c500a9b8e6d3432523529229078d4678
llp5030 <llp5030@IGM2350-21>
2019-04-21 10:26:41 -0400

    Resized Dungeon and Set a Hard-Coded Boss Room

--- WarrenWarriorsGame/WarrenWarriorsGame/Dungeon.cs
+++ WarrenWarriorsGame/WarrenWarriorsGame/Dungeon.cs
@@ -25,13 +25,13 @@ namespace WarrenWarriorsGame
 		public Dungeon(Game g)
 		{
 			int roller;
-			roomButtons = new Button[20, 20];
-			dungeonlayout = new Encounter[20, 20];
+			roomButtons = new Button[7, 7];
+			dungeonlayout = new Encounter[7, 7];
 
 			//Creates Buttons in Array
-			for (int x = 0; x < 20; x++)
+			for (int x = 0; x < 7; x++)
 			{
-				for (int y = 0; y < 20; y++)
+				for (int y = 0; y < 7; y++)
 				{
 					//Generates a random encounter difficulty for each room
 					roller = rn.Next(0, 5);
@@ -43,23 +43,26 @@ namespace WarrenWarriorsGame
 							break;
 						case 1:
 							dungeonlayout[x, y] = new Encounter(g, Difficulty.Easy);
-							roomButtons[x, y] = new Button(g.Content.Load<Texture2D>("RoomNormal"), g.Content.Load<Texture2D>("RoomNext"), g.Content.Load<Texture2D>("RoomCleared"), new Rectangle(x * 20, y * 20, 15, 15));
+							roomButtons[x, y] = new Button(g.Content.Load<Texture2D>("RoomNormal"), g.Content.Load<Texture2D>("RoomNext"), g.Content.Load<Texture2D>("RoomCleared"), new Rectangle(x * 60, y * 60, 60, 60));
 							break;
 						case 2:
 							dungeonlayout[x, y] = new Encounter(g, Difficulty.Medium);
-							roomButtons[x, y] = new Button(g.Content.Load<Texture2D>("RoomNormal"), g.Content.Load<Texture2D>("RoomNext"), g.Content.Load<Texture2D>("RoomCleared"), new Rectangle(x * 20, y * 20, 15, 15));
+							roomButtons[x, y] = new Button(g.Content.Load<Texture2D>("RoomNormal"), g.Content.Load<Texture2D>("RoomNext"), g.Content.Load<Texture2D>("RoomCleared"), new Rectangle(x * 60, y * 60, 60, 60));
 							break;
 						case 3:
 							dungeonlayout[x, y] = new Encounter(g, Difficulty.Hard);
-							roomButtons[x, y] = new Button(g.Content.Load<Texture2D>("RoomNormal"), g.Content.Load<Texture2D>("RoomNext"), g.Content.Load<Texture2D>("RoomCleared"), new Rectangle(x * 20, y * 20, 15, 15));
+							roomButtons[x, y] = new Button(g.Content.Load<Texture2D>("RoomNormal"), g.Content.Load<Texture2D>("RoomNext"), g.Content.Load<Texture2D>("RoomCleared"), new Rectangle(x * 60, y * 60, 60, 60));
 							break;
 						case 4:
 							dungeonlayout[x, y] = new Encounter(g, Difficulty.Boss);
-							roomButtons[x, y] = new Button(g.Content.Load<Texture2D>("RoomNormal"), g.Content.Load<Texture2D>("RoomNext"), g.Content.Load<Texture2D>("RoomCleared"), new Rectangle(x * 20, y * 20, 15, 15));
+							roomButtons[x, y] = new Button(g.Content.Load<Texture2D>("RoomNormal"), g.Content.Load<Texture2D>("RoomNext"), g.Content.Load<Texture2D>("RoomCleared"), new Rectangle(x * 60, y * 60, 60, 60));
 							break;
 					}
 				}
 			}
+
+            dungeonlayout[6, 6] = new Encounter(g, Difficulty.Boss);
+            roomButtons[6, 6] = new Button(g.Content.Load<Texture2D>("RoomNormal"), g.Content.Load<Texture2D>("RoomNext"), g.Content.Load<Texture2D>("RoomCleared"), new Rectangle(6 * 60, 6 * 60, 60, 60));
 		}
 
 		//Update Method
@@ -67,9 +70,9 @@ namespace WarrenWarriorsGame
 		//If it is, generates an encounter
 		public Encounter Update(MouseState ms, MouseState prev)
 		{
-			for (int x = 0; x < 20; x++)
+			for (int x = 0; x < 7; x++)
 			{
-				for (int y = 0; y < 20; y++)
+				for (int y = 0; y < 7; y++)
 				{
 					if (roomButtons[x, y] != null)
 					{
@@ -88,9 +91,9 @@ namespace WarrenWarriorsGame
 		//Draws the array of buttons
 		public void Draw(SpriteBatch sb)
 		{
-			for (int x = 0; x < 20; x++)
+			for (int x = 0; x < 7; x++)
 			{
-				for (int y = 0; y < 20; y++)
+				for (int y = 0; y < 7; y++)
 				{
 					if (roomButtons[x, y] != null)
 					{
@@ -103,7 +106,7 @@ namespace WarrenWarriorsGame
 
         public bool GameWin()
         {
-            if (dungeonlayout[19,19].CombatHandler.EnemyHealth() == 0)
+            if (dungeonlayout[6,6].CombatHandler.EnemyHealth() == 0)
             {
                 return true;
             }
